dfb3118345a04a9d7a821d31deefff5d35bd6738
Jorgebillini <Jorgebillini@pursuit.org>
2019-02-03 16:42:06 -0500

    solved merge conflict

--- helpers/operations.js
+++ helpers/operations.js
@@ -11,21 +11,30 @@ MULTIPLIES  by -1 or +
 Take num from state and perform operation 
 
 */
-
 const operation = {};
 
-operation.result = (num1,num2,opStr) =>{
-    if (opStr === '*'){
-        return multiply(num1,num2)
-    }
-   else if (opStr === '+'){
-        return add(num1,num2);
-    }
-    else if (opStr === '-'){
-        return substract(num1,num2);
-    }
-    else if (opStr === '/'){
-        return divide(num1,num2);
+const add = (num1, num2) => num1 + num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const divide = (num1, num2) => (num1 / num2);
+
+operation.percentage = (num) => num / 100;
+
+operation.convertPos_Neg = (num) => num * -1;
+
+operation.result = (num1, num2, opStr) => {
+    if (opStr === '*') {
+        return multiply(num1, num2)
+    } else if (opStr === '+') {
+        return add(num1, num2);
+    } else if (opStr === '-') {
+        return substract(num1, num2);
+    } else if (opStr === '/') {
+        return divide(num1, num2);
     }
 }
 
+export default operation;
