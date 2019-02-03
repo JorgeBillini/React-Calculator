7680db4e837351cf366ea3f4237dae6a3b7e9e7d
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-03 18:09:01 -0500

    added extraOps component

---
+++ src/components/extraOps.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ExtraOps extends React.Component{
+    render (){
+        return 
+        <>
+        <h1>AC</h1>
+        <h1>%</h1>
+        <h1>±</h1>
+        </>
+    }
+}
+
+export default ExtraOps;
