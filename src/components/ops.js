534b1a012677af1bf5fbe1f4988401201e27e68f
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-03 18:18:22 -0500

    created ops component

---
+++ src/components/ops.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Ops extends React.Component {
+    render(){
+        return (
+            
+            <h1>Display Operations</h1>
+            
+        );
+    } 
+}
+
+export default Ops;
