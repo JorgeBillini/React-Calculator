7997d8eca241f7240c6ba7f6b35d95edd66cf206
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-03 18:11:33 -0500

    added display component

---
+++ src/components/display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Display extend React.Component {
+    render(){
+        return (
+            <>
+            <h1>Display Num!</h1>
+            </>
+        );
+    } 
+}
+
+export default Display;
