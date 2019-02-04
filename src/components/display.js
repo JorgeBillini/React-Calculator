30122e73c31469ac41ea0b82655f84fc4f97eb56
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-03 22:26:26 -0500

    added display with basic styles

--- src/components/display.js
+++ src/components/display.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+const jumbo = {
+    padding: '100px',
+    backgroundColor: '#52536d',
+    fontSize: '100px',
+    textAlign: 'right',
+    color: 'white'
+}
+
 class Display extends React.Component {
     render(){
-        return (
-            
-            <h1>Display Num!</h1>
-            
+        return ( 
+            <h1 style={jumbo}>{this.props.displayVal}</h1>
         );
     } 
 }
