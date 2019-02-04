b6770ddb373539132e225f1ddd8914602550f072
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-03 22:30:48 -0500

    added initial multiplication

--- src/components/ops.js
+++ src/components/ops.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+/* <p style={button}>*</p> */
+const pStyle = {
+    fontSize: '200px',
+    textAlign: 'center',
+    color: '#52536d'
+
+};
+
+const dStyle = {
+    margin: '20px 30px',
+    width: '300px',
+    height: '250px',
+    // borderRadius: '30px',
+    // border: '10px solid #52536d',
+    borderLeft: '10px solid #52536d',
+    borderBottom: '10px solid #52536d',
+
+}
+
 class Ops extends React.Component {
+
+    handleMultiplyClick = () => {
+        this.props.addOperation('*');
+    }
+
     render(){
         return (
-            <h1>Display Operations</h1>
+            <div className='' style={dStyle}>
+                <p onClick={this.handleMultiplyClick} style={pStyle}>*</p>
+            </div>
         );
     } 
 }
