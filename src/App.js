7ac7879a762da13a67b72e11895c41f3f99aa715
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-05 00:35:23 -0500

    added basic methods for ops.js to modify state; no logic; and created Display jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Operation from './helpers/operations';
-import ExtraOPs from './components/extraOps'
+// import Operation from './helpers/operations';
+import ExtraOPs from './components/extraOps';
 import Display from './components/display';
-import Numbers from './components/numbers';
+// import Numbers from './components/numbers';
 import Ops from './components/ops';
 
 class App extends React.Component {
@@ -16,13 +16,48 @@ class App extends React.Component {
         }
     }
 
+    /*
+    const {displayVal, previousVal, operation, waitingForNewValue} = this.state;
+
+
+if(operation is clicked){
+    if(operation && waitingForNewValue){
+
+    }
+
+    if(operation && waitingForNewValue){
+        this.setState({operation: opStr})
+    }
+
+     this.setState({waitingForNewValue:true})
+}
+
+if(num is clicked){
+    if(!displayVal){
+        this.setState({displayVal: num})
+    } else {
+        const newDisplayValue = displayVal + num;
+        this.setState({displayVal: newDisplayValue,)
+    }
+
+    this.setState({waitingForNewValue:false})
+}
+
+*/
+
+    addOperation = (opStr) => {
+        const {operation} = this.state;
+        this.setState({operation: opStr});
+      }
+
     render(){
+        console.log(this.state);
         return (
             <>
-                <Display />
-                <ExtraOPs />
-                <Ops />
-                <Numbers /> 
+                <Display displayVal={this.state.displayVal}/>
+                {/* <ExtraOPs /> */}
+                <Ops addOperation={this.addOperation}/>
+                {/* <Numbers />  */}
             </>
         );
     }
