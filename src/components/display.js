dff2789cd4d63fb2821a84416eb08858f66fc61c
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-10 00:18:01 -0500

    added styling to display component

--- src/components/display.js
+++ src/components/display.js
@@ -1,17 +1,9 @@
 import React from 'react';
 
-const jumbo = {
-    padding: '100px',
-    backgroundColor: '#52536d',
-    fontSize: '100px',
-    textAlign: 'right',
-    color: 'white'
-}
-
 class Display extends React.Component {
     render(){
         return ( 
-            <h1 style={jumbo}>{ this.props.displayVal}</h1>
+            <h1 className='display beige'>{this.props.displayVal}</h1>
         );
     } 
 }
